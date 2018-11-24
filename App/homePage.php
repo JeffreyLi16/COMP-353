a4054f3b2c2649ef70c4337bd84dda6f6842033a
StephiieC <stephanie.chiang@hotmail.com>
2018-11-24 02:19:00 -0500

    testing

--- App/homePage.php
+++ App/homePage.php
@@ -10,21 +10,23 @@
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">   
 </head>
    <body>
-   <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-    </ul>
-  </div>
-</nav>
-      <div id="main-wrapper"><h2><center>welcome <?php echo $firstName;?></center></h2>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
 
-      </div>
+        <ul class="nav navbar-nav navbar-right">
+          <!-- <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
+          <li><a href="userInfo.php"><span class="glyphicon glyphicon-user"></span> Setting</a></li>
+          <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+        </ul>
+    </div>
+    </nav>
+
+
+      <div id="main-wrapper"><h2><center>welcome <?php echo $firstName;?></center></h2></div>
    </body>
 </html>
 
