cfb8e28a82d9ad231975356cfa5497427a8b9870
Abishand <35385222+AA-97@users.noreply.github.com>
2018-11-24 01:04:28 -0500

    added navbar

--- App/employeeHomePage.php
+++ App/employeeHomePage.php
@@ -7,5 +7,29 @@
     $ID = $_SESSION["employeeID"];
     $firstName = $_SESSION['FirstName'];
     $lastName = $_SESSION['LastName'];
-    echo($ID);
 ?>
+
+<html>
+<head>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">   
+</head>
+   <body>
+   <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="employeeLogin.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+    </ul>
+  </div>
+</nav>
+      <div id="main-wrapper"><h2><center>welcome <?php echo $firstName;?></center></h2>
+
+      <div><a href="">Create client account</a></div>
+
+      </div>
+   </body>
+</html>
