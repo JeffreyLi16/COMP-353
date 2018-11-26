0fc31e76b24945f8779522b9ba322db302fa95dc
StephiieC <stephanie.chiang@hotmail.com>
2018-11-26 01:41:20 -0500

    replace home nav as BOC

--- App/openClientAccount.php
+++ App/openClientAccount.php
@@ -68,7 +68,7 @@
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Bank of Concordia</a>
+    <a class="navbar-brand" href="employeeHomePage.php">Bank of Concordia</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -76,9 +76,6 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="employeeHomePage.php">Home <span class="sr-only">(current)</span></a>
-        </li>
         <li class="nav-item">
           <a class="nav-link" href="openClientAccount.php">Open Account</a>
         </li>
